347896fb77d0fc209ac9a700e54f671a35110104
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2024-07-21 14:22:52 +0200

    putting some order in my project

---
+++ personalities-business/src/main/java/com/personalities/demo/PersonalitiesBusinessApplication.java
@@ -0,0 +1,13 @@
+package com.personalities.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalitiesBusinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalitiesBusinessApplication.class, args);
+	}
+
+}
