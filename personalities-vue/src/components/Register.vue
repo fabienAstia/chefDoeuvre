6d249d0a6658d3829d70ad30b47ec6d5f2cf7b7b
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2024-11-13 14:38:49 +0100

    vuelidate

--- personalities-vue/src/components/Register.vue
+++ personalities-vue/src/components/Register.vue
@@ -4,6 +4,7 @@ import { validateRegistration } from './validateRegistration';
 const {t} = useI18n;
 
 const {
+  validatedRegisterForm,
   registerForm,
   handleSubmit,
   v$
@@ -26,23 +27,22 @@ const emit = defineEmits(['registered']);
         <div class="modal-body text-bg-light" >
             <div class="form-group">
               <label for="username" class="col-form-label">{{$t('register.username')}} :</label>
-              <input type="text" class="form-control" id="username" v-model="registerForm.username">
-              <span v-for="error in v$.username.$errors" :key="error.$uid" class="text-danger fw-bold">{{ error.$message }}</span>
-
+              <input type="text" class="form-control" id="username" v-model="registerForm.username" @blur="v$.username.$touch()">   
+              <span v-for=" error in v$.username.$errors"   class="text-danger">{{ error.$message }}</span>
             </div>
             <div class="form-group">
               <label for="email" class="col-form-label">{{$t('register.email')}} :</label>
-              <input type="email" class="form-control" id="email" v-model="registerForm.email">
-              <span v-for="error in v$.email.$errors" :key="error.$uid" class="text-danger fw-bold">{{ error.$message }}</span>
+              <input type="email" class="form-control" id="email" v-model="registerForm.email" @blur="v$.email.$touch()">
+              <span v-for="error in v$.email.$errors" :key="error.$uid" class="text-danger"><div>{{ error.$message }}</div></span>
             </div>
             <div class="form-group">
               <label for="password" class="col-form-label">{{$t('register.password')}} :</label>
-              <input type="password" class="form-control" id="password" v-model="registerForm.password"></input>
-              <span v-for="error in v$.password.$errors" :key="error.$uid" class="text-danger fw-bold">{{ error.$message }}</span>
+              <input type="password" class="form-control" id="password" v-model="registerForm.password" @blur="v$.password.$touch()">
+              <span v-for="error in v$.password.$errors" :key="error.$uid" class="text-danger "><div>{{ error.$message }}</div></span>
             </div>
         </div>
         <div class="modal-footer text-bg-light">
-          <button type="submit" class="btn btn-secondary">{{$t('register.submit')}}</button>
+          <button type="submit" class="btn btn-secondary" >{{$t('register.submit')}}</button>
         </div>
     </form>
 
--- personalities-vue/src/components/validateRegistration.js
+++ personalities-vue/src/components/validateRegistration.js
@@ -1,6 +1,6 @@
-import {reactive, computed} from 'vue';
+import {ref, reactive, computed} from 'vue';
 import { useVuelidate } from '@vuelidate/core'
-import { required, helpers, maxLength, minLength, between } from '@vuelidate/validators'
+import { required, helpers, maxLength, minLength } from '@vuelidate/validators'
 
 
 export function validateRegistration(){
@@ -12,61 +12,55 @@ export function validateRegistration(){
       
     const emailPattern = /^(?=.{1,64}@)\w+([\.-]?\w+)*@(?=.{4,252}$)\w+([\.-]?\w+)*(\.\w{2,4})+$/;
     const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])\S{8,16}$/;
-    const containsUppercase = (registerForm) => {
-        return /[A-Z]/.test(registerForm)
-    }
-      
+
+    const upperCase = /(?=.*[A-Z])/;
+    const lowerCase = /(?=.*[a-z])/;
+    const digit = /(?=.*\d)/;
+    const specialChar = /(?=.*[^\w\s])/;
+
+    const validEmail = (registerForm) => emailPattern.test(registerForm);
+    const containsUppercase = (registerForm) => upperCase.test(registerForm);
+    const containsLowercase = (registerForm) => lowerCase.test(registerForm);
+    const containsDigit = (registerForm) => digit.test(registerForm);
+    const containSpecialChar = (registerForm) => specialChar.test(registerForm);
+
     const rules = computed(() =>{
         return {
             username: { 
-                required: helpers.withMessage("This field cannot be empty", required),
-                minLength: helpers.withMessage("Username must contain at least 3 characters.",  minLength(3)),
-                maxLength: helpers.withMessage("Username must not contain most than 20 characters", maxLength(20))
+                required: helpers.withMessage("Username is required", required),
+                minLength: minLength(3),
+                maxLength: maxLength(20)
             }, 
             email: { 
-                required: helpers.withMessage("This field cannot be empty", required), 
-                minLength: helpers.withMessage("Email must contain at least 4 characters", minLength(4))
+                required: helpers.withMessage("Email is required", required), 
+                validMail: helpers.withMessage("Email must be valid", validEmail)
             }, 
             password: {
-                required: helpers.withMessage("This field cannot be empty", required), 
-                minLength: helpers.withMessage("Password must contain at least 8 charactesr", minLength(8)), 
-                maxLength: helpers.withMessage("Password must not exceed 16 characters", maxLength(16)), 
-                // containsUppercase: helpers.withMessage("Password must contain 1 uppercase", containsUppercase),
-
-                // containsUppercase: helpers.withMessage(
-                //     () => `The password requires an uppercase`,
-                //     (registerForm) => /[A-Z]/.test(registerForm)
-                // ),
-                containsUppercase: function(registerForm){
-                    return /[A-Z]/.test(registerForm)
-                },
-                containsUppercase: function(registerForm) {
-                    return /[A-Z]/.test(registerForm)
-                },
-                containsLowercase: function(registerForm) {
-                    return /[a-z]/.test(registerForm)
-                },
-                containsNumber: function(registerForm) {
-                    return /[0-9]/.test(registerForm)
-                },
-                containsSpecial: function(registerForm) {
-                    return /[#?!@$%^&*-]/.test(registerForm)
-                },
-                // containsPasswordRequirement: helpers.withMessage(
-                //     () => `The password requires an uppercase, lowercase, number and special character`, 
-                //     (value) => /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])/.test(value)
-                // )
+                required: helpers.withMessage("Password is required", required), 
+                minLength: minLength(8),
+                maxLength: maxLength(16),
+                withUppercase: helpers.withMessage("Password must contain 1 uppercase", containsUppercase),
+                withLowercase: helpers.withMessage("Password must contain 1 lowercase", containsLowercase),
+                withDigit: helpers.withMessage("Password must contain 1 digit", containsDigit),
+                withSpecialChar: helpers.withMessage("Password must contain 1 special character", containSpecialChar)
             }
         }
     })
 
+    function touchField(field) {
+        field.value.$touch();
+    }
+
+    const validatedRegisterForm = ref(false);
+
     async function handleSubmit() {
-        const result = await v$.value.$validate()
+        const result = await v$.value.$touch() 
         if (!result) {
           alert('The form has errors')
-          return
+          return 
         }
         alert('Form submitted successfully');
+        validatedRegisterForm  = true;
         newRegistration();
     }
       
@@ -95,13 +89,15 @@ export function validateRegistration(){
     }
 
     return {
+        validatedRegisterForm,
         registerForm,
         emailPattern,
         passwordPattern,
         rules,
         handleSubmit,
         v$,
-        newRegistration
+        newRegistration,
+        touchField
     }
 }
 
