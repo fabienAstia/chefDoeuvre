0fd1b41e4faeb6aa360a934ee6721d81002300f7
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-03-19 21:45:15 +0100

    big refacto of result page

--- personalities-business/src/main/java/com/personalities/services/AnswerService.java
+++ personalities-business/src/main/java/com/personalities/services/AnswerService.java
@@ -11,8 +11,6 @@ import com.personalities.repositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class AnswerService {
@@ -33,82 +31,8 @@ public class AnswerService {
 
     public MbtiTypeView createAnswersAndGetResult(List<AnswerCreate> inputs) {
         createAnswers(inputs);
-        String mbtiCode = getMbtiType(inputs);
-        MbtiType mbtiType = mbtiTypeRepository.findProjectedByCode(mbtiCode);
-        return buildMbtiTypeView(mbtiType, inputs);
-    }
-
-    private MbtiTypeView buildMbtiTypeView(MbtiType mbtiType, List<AnswerCreate> inputs) {
-        return new MbtiTypeView(
-                mbtiType.getCode(),
-                mbtiType.getName(),
-                mbtiType.getDescription(),
-                mbtiType.getPopulationPercentage(),
-                mbtiType.getInterestingFact(),
-                mbtiType.getImage(),
-                mbtiType.getProfessions().stream().map(Profession::getName).toList(),
-                buildPersonalityTraitsWithEval(mbtiType),
-                getRateByPsych(inputs));
-    }
-
-    private Map<String, String> buildPersonalityTraitsWithEval(MbtiType mbtiType) {
-        Map<String, String> traitsWithEval = new HashMap<>();
-        mbtiType.getPersonalityTraits().forEach(personalityTrait -> {
-            traitsWithEval.putIfAbsent(personalityTrait.getTrait(), personalityTrait.getEvaluation().getLabel());
-        });
-        return traitsWithEval;
-    }
-
-    private String getMbtiType(List<AnswerCreate> inputs) {
-        Map<String, List<Integer>> ratingByPsych = getRatingByPsych(inputs);
-        Map<String, Integer> scoreByPsych = getScoreByPsych(ratingByPsych);
-        return getMbtiTypeCode(scoreByPsych);
-    }
-
-    private Map<String, Double> getRateByPsych(List<AnswerCreate> inputs) {
-        Map<String, List<Integer>> ratingByPsych = getRatingByPsych(inputs);
-        Map<String, Integer> scoreByPsych = getScoreByPsych(ratingByPsych);
-        Map<String, Double> rateByPsychPref = new HashMap<>();
-        scoreByPsych.forEach((key, value) -> rateByPsychPref.putIfAbsent(key, (((double) value / 24) * 100)));
-        return rateByPsychPref;
-    }
-
-    private static String getMbtiTypeCode(Map<String, Integer> scoreByPsych) {
-        return Stream.of(
-                        determinePreference(scoreByPsych, PsychPref.EXTRAVERSION, PsychPref.INTROVERSION),
-                        determinePreference(scoreByPsych, PsychPref.INTUITION, PsychPref.SENSATION),
-                        determinePreference(scoreByPsych, PsychPref.THINKING, PsychPref.FEELING),
-                        determinePreference(scoreByPsych, PsychPref.PERCEPTION, PsychPref.JUDGMENT))
-                .map(PsychPref::getPrefCode)
-                .collect(Collectors.joining());
-    }
-
-    private static PsychPref determinePreference(Map<String, Integer> scoreByPsych, PsychPref pref1, PsychPref pref2) {
-        return (scoreByPsych.get(pref1.getPrefCode()) >= scoreByPsych.get(pref2.getPrefCode())) ? pref1 : pref2;
-    }
-
-    public Map<String, List<Integer>> getRatingByPsych(List<AnswerCreate> answers) {
-        Map<String, List<Integer>> ratingByPsych = new HashMap<>();
-        List<Question> questions = questionRepository.findAll();
-        questions.forEach(question -> {
-            Optional<AnswerCreate> answer = answers.stream()
-                    .filter(a -> a.questionId().equals(question.getId()))
-                    .findFirst();
-            answer.ifPresent(a -> {
-                ratingByPsych.putIfAbsent(question.getPsychPreference().getCode(), new ArrayList<>());
-                ratingByPsych.get(question.getPsychPreference().getCode()).add(a.rating());
-            });
-        });
-        return ratingByPsych;
-    }
-
-    public Map<String, Integer> getScoreByPsych(Map<String, List<Integer>> ratingByPsych) {
-        Map<String, Integer> scoreByPsych = new HashMap<>();
-        ratingByPsych.forEach((key, value) -> {
-            Integer psychScore = value.stream().mapToInt(i -> i).sum();
-            scoreByPsych.put(key, psychScore);
-        });
-        return scoreByPsych;
+        ResultService resultService = new ResultService(questionRepository, mbtiTypeRepository);
+        return resultService.getResult(inputs);
     }
 
     private void createAnswers(List<AnswerCreate> inputs) {
--- personalities-vue/src/views/ResultView.vue
+++ personalities-vue/src/views/ResultView.vue
@@ -6,18 +6,26 @@
     const mbtiStore = useMbtiStore();
     const mbtiType = computed(() => mbtiStore.result);
 
-    const sortedTraits = () => {
-        const goodTraits = new Array();
-        const badTraits = new Array();
+    const sortedTraits = computed(() => {
+        const traits = {force: [], faiblesse: []};
         for(const[trait,evaluation] of Object.entries(mbtiType.value.personalityTraitsWithEval)){
-            if(evaluation==='Force'){
-                goodTraits.push(trait);
-            }else{
-                badTraits.push(trait);
+            if(evaluation === "Force"){
+                traits.force.push(trait);
+            }else {
+                traits.faiblesse.push(trait);
             }
         }
-        return {goodTraits, badTraits};
-    }
+        console.log("traits =" + traits)
+        return traits;
+    });
+
+    const percentages = computed(() => {
+        const percentages = {};
+        for(const[prefPsych, percentage] of Object.entries(mbtiType.value.rateByPsych)){
+            percentages[prefPsych] = percentage;
+        }
+        return percentages;
+    });
 
     const data = {
         labels: [
@@ -32,7 +40,7 @@
         ],
         datasets: [{
             label: 'Psychological preferences',
-            data: [33, 59, 90, 81, 56, 55, 40, 43],
+            data: [percentages.value["N"], percentages.value["T"], percentages.value["E"], percentages.value["P"], percentages.value["S"], percentages.value["F"], percentages.value["I"], percentages.value["J"]],
             fill: true,
             backgroundColor: 'rgba(255, 99, 132, 0.2)',
             borderColor: 'rgb(255, 99, 132)',
@@ -49,11 +57,14 @@
         options: {
             scales: {
                 r: {
+                    beginAtZero: true,
+                    min:0,
+                    max:100,
                     pointLabels: {
-                        display: true // Hides the labels around the radar chart 
+                        display: true 
                     },
                     ticks: {
-                        display: false // Hides the labels in the middle (numbers)
+                        display: false 
                     }
                 }
             },
@@ -76,7 +87,7 @@
     onMounted(async() => {
         const context = document.getElementById('myChart'); 
         const myChart = new Chart(context, config);
-        console.log("rateByPsych :" + Object.entries(mbtiType.value.rateByPsych.get('Intuition')))
+        console.log("rateByPsych :" + Object.entries(mbtiType.value.rateByPsych))
     })
 
 </script>
@@ -88,15 +99,14 @@
                 <h1 class="text-shadow mb-5"><b>Vos résultats</b></h1>
             </div>
         </div>
-        <div class="row align-items-center my-3">
+        <div class="row align-items-center my-3 style">
             <h3 class="text-center text-shadow-light">Présentation</h3>
-            <hr>
-            <div class="d-flex col-12 col-md-6 justify-content-center fs-6 fs-md-5" >
-                <ul class="fs-6 fs-md-5">
-                    <li class="my-1 text-center"><span class="text-shadow-light"><b>{{ mbtiType.code }} - {{ mbtiType.name }}</b></span></li>
-                    <li class="my-1">{{ mbtiType.description }}</li>
-                    <li class="my-1">{{ mbtiType.populationPercentage }}% of the population</li>
-                    <li class="my-1">{{ mbtiType.interestingFact }}</li>
+            <div class="col-12 col-md-6 justify-content-center fs-6 fs-md-5" >
+                <div class="text-center text-shadow-light w-100 mb-3"><b>{{ mbtiType.code }} - {{ mbtiType.name }}</b></div>
+                <ul class="fs-6 fs-md-5" style="list-style-type: '▸';">
+                    <li class="my-3 ms-3">{{ mbtiType.description }}</li>
+                    <li class="my-3 ms-3">{{ mbtiType.populationPercentage }}% of the population</li>
+                    <li class="my-3 ms-3">{{ mbtiType.interestingFact }}</li>
                 </ul>
             </div>
             <div class="d-flex col-12 col-md-6 justify-content-center">
@@ -104,39 +114,92 @@
             </div>
         </div>
 
-        <div class="row align-items-center my-3">
+        <div class="row align-items-center my-5 style">
             <h3 class="text-center text-shadow-light">Vos résultats</h3>
-            <hr>
             <div class="d-flex col-12 col-md-6 justify-content-center" >
                 <canvas id="myChart"></canvas> 
             </div>
-            <div class="d-flex col-12 col-md-6 justify-content-center">
-                <ul>
-                    <li class="my-1">Intoversion / Extraversion - {{ Object.entries(mbtiType.rateByPsych) }}</li>
-                    <li class="my-1">Intuition / Sensation</li>
-                    <li class="my-1">Pensée / Sentiment</li>
-                    <li class="my-1">Perception / Jugement</li>
+            <div class="d-flex col-12 col-md-6 justify-content-center text-center">
+                <ul class="w-100 no-bullet">
+                    <li class="my-3">
+                        <div class="d-flex">
+                            <div style="color: #0077b6;">
+                                <b>Introversion</b> 
+                            </div>
+                            <div class="text-end w-100" style="color: #270446;">
+                                <b>Extraversion</b>
+                            </div>
+                        </div>
+                        <div class="progress position-relative" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar" :style="{ width: `${percentages.I}%` }"></div>
+                            <span class="position-absolute w-100 text-white text-start">{{percentages["I"] }}%</span> 
+                            <span class="position-absolute w-100 text-white text-end"> {{ percentages["E"] }}%</span>
+                        </div>
+                    </li>
+                    <li class="my-3">
+                        <div class="d-flex">
+                            <div style="color: #0077b6;">
+                                <b>Intuition</b> 
+                            </div>
+                            <div class="text-end w-100" style="color: #270446;">
+                                <b>Sensation</b>
+                            </div>
+                        </div>
+                        <div class="progress position-relative" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar" :style="{ width: `${percentages.N}%` }"></div>
+                            <span class="position-absolute w-100 text-white text-start">{{percentages["N"] }}%</span> 
+                            <span class="position-absolute w-100 text-white text-end"> {{ percentages["S"] }}%</span>
+                        </div>
+                    </li>
+                    <li class="my-3">
+                        <div class="d-flex">
+                            <div style="color: #0077b6;">
+                                <b>Pensée</b> 
+                            </div>
+                            <div class="text-end w-100" style="color: #270446;">
+                                <b>Sentiment</b>
+                            </div>
+                        </div>
+                        <div class="progress position-relative" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar" :style="{ width: `${percentages.T}%` }"></div>
+                            <span class="position-absolute w-100 text-white text-start">{{ percentages["T"] }}%</span> 
+                            <span class="position-absolute w-100 text-white text-end"> {{ percentages["F"] }}%</span>
+                        </div>
+                    </li>
+                    <li class="my-3">
+                        <div class="d-flex">
+                            <div style="color: #0077b6;">
+                                <b>Perception</b> 
+                            </div>
+                            <div class="text-end w-100" style="color: #270446;">
+                                <b>Jugement</b>
+                            </div>
+                        </div>
+                        <div class="progress position-relative" role="progressbar" aria-label="Info example" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                            <div class="progress-bar" :style="{ width: `${percentages.P}%` }"></div>
+                            <span class="position-absolute w-100 text-white text-start">{{ percentages["P"] }}%</span> 
+                            <span class="position-absolute w-100 text-white text-end"> {{ percentages["J"] }}%</span>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>
 
-        <div class="row text-center">
+        <div class="row text-center mt-5 style">
             <h3 class="text-shadow-light">Forces et Faiblesses</h3>
-            <hr>
             <div class="col-12 col-md-6">
-                    <h5 class="text-shadow-light my-3">Forces</h5>
-                    <div class="mb-1" v-for="trait in sortedTraits().goodTraits" :key="trait">{{ trait }}</div>
+                    <h5 class="text-shadow-light mb-3">Forces</h5>
+                    <div class="mb-1" v-for="trait in sortedTraits.force" :key="trait">{{ trait }}</div>
             </div>
             <div class="col-12 col-md-6">
-                    <h5 class="text-shadow-light my-3">Faiblesses</h5>
-                    <div class="mb-1" v-for="trait in sortedTraits().badTraits" :key="trait">{{ trait }}</div>
+                    <h5 class="text-shadow-light mb-3">Faiblesses</h5>
+                    <div class="mb-1" v-for="trait in sortedTraits.faiblesse" :key="trait">{{ trait }}</div>
             </div>
         </div>
 
-        <div class="row mt-3">
+        <div class="row mt-5 style">
+            <h3 class="text-center text-shadow-light">Métiers</h3>
             <div class="col">
-                <h4 class="text-center text-shadow-light my-3">Métiers</h4>
-                <hr>
                 <div class="text-center mb-1" v-for="job in mbtiType.professions">{{ job }}</div>
             </div>
         </div>
@@ -145,20 +208,31 @@
 </template>
 
 <style scoped>
+html{
+    background-color: #effcfe;
+}
 body{
     max-width: 900px!important;
-    background-color: #effcfe;;
     padding: 15px;
 }
 ul{
     padding: 0;
 }
-li{
-    list-style-type: none;
-}
 h1{
     font-size: 4em;
     color: #0077b6;
+}
+h3{
+    margin-bottom: 25px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+    border-bottom: 2px solid #0079bb21;
+    background-color: white;
+    border: 2px solid white;
+}
+.style{
+    background-color: #effcfe;
+    border: 2px solid #0079bb21;
 }
 .text-shadow{
     text-shadow:
@@ -195,6 +269,18 @@ h1{
 #myChart{
     max-width: 350px;
     max-height: 300px;
+    min-height: 100px;
+}
+.progress-bar{
+    background-color: #0077b6;
+    border-color: #00344f;
+}
+.progress{
+    background-color:#4e038f;
+    border-color: #270446;
+}
+.no-bullet{
+    list-style: none;
 }
 
 </style>
