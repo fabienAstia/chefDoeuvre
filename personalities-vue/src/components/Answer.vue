57ca677509aa84c6ecd46357789865f9817ef5fe
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-04-26 22:49:43 +0200

    trying to render offerJobList with innerHtml and loop

--- personalities-vue/src/components/Answer.vue
+++ personalities-vue/src/components/Answer.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, onMounted, computed} from 'vue';
+import {ref, onMounted} from 'vue';
 import{useI18n} from 'vue-i18n';
 const{t} = useI18n;
 import { useQuestions } from '../composables/useQuestions';
