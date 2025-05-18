0fe47f963529223bdf8e81da45259314dfb9a324
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-05-18 16:40:05 +0200

    fixing useState

--- personalities-vue/src/composables/useState.js
+++ personalities-vue/src/composables/useState.js
@@ -1,7 +1,10 @@
 import {ref} from 'vue';
 
 const sharedState = ref('');
-
+const isLogged = () => {
+    
+    // ref(localStorage.getItem('token'))
+}
 export function useSharedState(){
     return sharedState;
 }
