0f99587931f292bd5becced2bf09b8a952134f1b
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-06-29 11:15:42 +0200

    cache on backend + address displayed

--- personalities-business/src/main/java/co/simplon/personalities/PersonalitiesBusinessApplication.java
+++ personalities-business/src/main/java/co/simplon/personalities/PersonalitiesBusinessApplication.java
@@ -2,8 +2,10 @@ package co.simplon.personalities;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class PersonalitiesBusinessApplication {
 
     public static void main(String[] args) {
