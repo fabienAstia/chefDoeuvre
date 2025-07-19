ce990bd9a96ee698a63739fc9b668fe657689913
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-07-19 17:07:50 +0200

    changing msg of homeView + commenting specificJobs details on Personality.vue

--- personalities-vue/src/views/HomeView.vue
+++ personalities-vue/src/views/HomeView.vue
@@ -1,18 +1,22 @@
 <script setup>
 import arrowCircle from '@/assets/pictos/arrowCircle.svg';
-
+import image_background from '@/assets/images/home_img.png';
 </script>
 
 
 <template>
 
     <div class="d-flex justify-content-center body">
+        <!-- <div class="container g-3 m-3" :style="{backgroundImage : `url(${image_background})`}"> -->
         <div class="container g-3 m-3">
 
-            <h1 class="text-center">« C’est formidable d’être enfin compris. »</h1>
-            <p class="my-3 py-3 fs-4 text-center">
-                Obtenez en 10 minutes seulement une description « incroyablement exacte » de qui vous êtes et des 
-                raisons qui sous-tendent vos agissements.
+            <h1 class="text-center">« Mieux vous comprendre, c’est déjà avancer. »</h1>
+            <p class="my-3 pt-3 fs-4 text-center">
+                En 10 minutes chrono, découvrez un aperçu précis de votre personnalité
+                et des raisons qui sous-tendent vos agissements.
+            </p>
+            <p class="my-3 pb-3 fs-4 text-center">
+                C’est la première application qui connecte votre type MBTI à des opportunités professionnelles faites pour vous.
             </p>
 
             <div class=" d-flex justify-content-center">
