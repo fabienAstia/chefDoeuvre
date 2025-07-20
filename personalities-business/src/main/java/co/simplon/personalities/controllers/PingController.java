ef759a9da6a82d7c45779aa887835cd7cb47653f
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-07-20 21:31:52 +0200

    adding ping endpoint to SecurityConfiguration to bypass + encoding in UTF-8 application.properties

---
+++ personalities-business/src/main/java/co/simplon/personalities/controllers/PingController.java
@@ -0,0 +1,18 @@
+package co.simplon.personalities.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ping")
+public class PingController {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String ping() {
+        return "connected";
+    }
+}
