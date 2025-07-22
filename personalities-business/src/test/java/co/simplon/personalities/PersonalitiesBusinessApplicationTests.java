ba225931e098cb897c8fbb5af2448c0fc122aab0
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-07-22 21:47:28 +0200

    add annotation @ActivteProfiles(dev)

--- personalities-business/src/test/java/co/simplon/personalities/PersonalitiesBusinessApplicationTests.java
+++ personalities-business/src/test/java/co/simplon/personalities/PersonalitiesBusinessApplicationTests.java
@@ -2,7 +2,9 @@ package co.simplon.personalities;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("dev")
 @SpringBootTest
 class PersonalitiesBusinessApplicationTests {
 
