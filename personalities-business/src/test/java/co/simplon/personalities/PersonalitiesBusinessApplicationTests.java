d497c0d1e57e8ab09e38f63d7775257e27b698f7
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-07-29 16:49:19 +0200

    removing @ActiveProfile(test) in order to deploy withou pb

--- personalities-business/src/test/java/co/simplon/personalities/PersonalitiesBusinessApplicationTests.java
+++ personalities-business/src/test/java/co/simplon/personalities/PersonalitiesBusinessApplicationTests.java
@@ -2,9 +2,7 @@ package co.simplon.personalities;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("dev")
 @SpringBootTest
 class PersonalitiesBusinessApplicationTests {
 
