d722367deed18640df0224f7e8855ac3340f1d40
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-08-07 11:00:10 +0200

    trying to Stub AnswerService

--- personalities-business/src/test/java/co/simplon/personalities/controllers/AnswerControllerTest.java
+++ personalities-business/src/test/java/co/simplon/personalities/controllers/AnswerControllerTest.java
@@ -1,12 +1,12 @@
 package co.simplon.personalities.controllers;
 
 import co.simplon.personalities.dtos.AnswerCreate;
+import co.simplon.personalities.services.AnswerService;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
@@ -15,15 +15,21 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 @TestPropertySource("classpath:application-dev.properties")
 class AnswerControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private AnswerService service;
 
     @ParameterizedTest
     @CsvFileSource(resources = "/submitAnswersAndGetResult_OK.csv", numLinesToSkip = 1)
@@ -34,6 +40,8 @@ class AnswerControllerTest {
                 .content(json)
                 .contentType(MediaType.APPLICATION_JSON);
         ResultActions result = mockMvc.perform(builder);
+        when(service.submitAnswersAndGetResult(List.of())).thenReturn()
+
         result.andExpect(status().isCreated());
     }
 }
