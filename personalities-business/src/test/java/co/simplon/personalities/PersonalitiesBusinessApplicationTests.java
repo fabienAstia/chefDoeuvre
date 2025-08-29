ea8fb6e4ce0c736e32554a01e41d84a3bc47d4b1
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-08-29 11:18:05 +0200

    moving @Disabled to @ActiveProfiles(test) on PersonalitiesBusinessApllicationTests

--- personalities-business/src/test/java/co/simplon/personalities/PersonalitiesBusinessApplicationTests.java
+++ personalities-business/src/test/java/co/simplon/personalities/PersonalitiesBusinessApplicationTests.java
@@ -1,10 +1,11 @@
 package co.simplon.personalities;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@Disabled("Disabled for CI/CD build to avoid DB errors")
+//@Disabled("Disabled for CI/CD build to avoid DB errors")
+@ActiveProfiles("test")
 @SpringBootTest
 class PersonalitiesBusinessApplicationTests {
 
