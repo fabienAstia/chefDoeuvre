ce06eba5c2a03357b26fba0be6dfb7cf1edca8b5
Fabien Astiasaran <fabien.astiasaran@gmail.com>
2025-08-29 11:20:14 +0200

    removing smell code

--- personalities-business/src/test/java/co/simplon/personalities/PersonalitiesBusinessApplicationTests.java
+++ personalities-business/src/test/java/co/simplon/personalities/PersonalitiesBusinessApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-//@Disabled("Disabled for CI/CD build to avoid DB errors")
 @ActiveProfiles("test")
 @SpringBootTest
 class PersonalitiesBusinessApplicationTests {
